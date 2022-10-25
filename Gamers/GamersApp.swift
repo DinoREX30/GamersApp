52e329f160fc2b44ab25e396d3035e38618dd9d3
Raúl <baltazardominguez.raul@outlook.com>
2022-10-25 09:21:43 -0500

    Initial Commit

---
+++ Gamers/GamersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamersApp.swift
+//  Gamers
+//
+//  Created by Raúl Baltazar Domínguez on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GamersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
